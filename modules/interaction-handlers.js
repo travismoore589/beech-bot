6908075e9b58a86690f2215ca104ffcf07830cd2
travismoore589 <68181253+travismoore589@users.noreply.github.com>
2024-03-07 10:29:18 -0500

    add avatarHandler

--- modules/interaction-handlers.js
+++ modules/interaction-handlers.js
@@ -3,6 +3,28 @@ const queries = require('../database/queries.js');
 
 module.exports = {
 
+    avatarHandler: async function sendMessage (message) {
+        const embed = new EmbedBuilder()
+        .setColor("Blurple")
+        .setDescription(message);
+
+        await interaction.reply({ embeds: [embed], ephermeral: true })
+
+        if (interaction.user.id !== '607017300989706240, 641147500908118016') return await interaction.reply({ content: `You do not have permission to use this command!`, ephermeral: true });
+
+    var error; 
+    await client.user.setAvatar(avatar.url).catch(async err => {
+        error = true; 
+        console.log(err);
+        return await sendMessage(`⚠️ Error : \`${err.toString()}\``);
+    });
+
+    if (error) return; 
+    await sendMessage(`Avatar updated successfully!`);
+
+
+    },
+
     addHandler: async (interaction) => {
         const author = interaction.options.getString('author').trim();
         const quote = interaction.options.getString('quote').trim();
