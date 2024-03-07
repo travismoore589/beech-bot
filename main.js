79aec998ecb6624a710d92efca5df067ac932662
travismoore589 <68181253+travismoore589@users.noreply.github.com>
2024-03-07 11:12:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Intents } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const BOT = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
@@ -42,3 +43,37 @@ BOT.on('interactionCreate', async interaction => {
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
+
+module.exports = {
+    owner: true,
+    data: new SlashCommandBuilder()
+    .setName('setavatar')
+    .setDescription('Set the avatar of the bot')
+    .addAttachmentOption(option => option.setName('avatar').setDescription('An image to use as the avatar for the bot').setRequired(true)),
+    async execute (interaction, client) {
+
+        const { options } = interaction;
+        const avatar = options.getAttachment('avatar');
+
+        async function sendMessage (message) {
+            const embed = new EmbedBuilder()
+            .setColor("Blurple")
+            .setDescription(message);
+
+            await interaction.reply({ embeds: [embed], ephermeral: true });
+        }
+
+        if (interaction.user.id !== '607017300989706240, 641147500908118016') return await interaction.reply({ content: `You do not have permission to use this command!`, ephermeral: true });
+
+        var error; 
+        await client.user.setAvatar(avatar.url).catch(async err => {
+            error = true; 
+            console.log(err);
+            return await sendMessage(`⚠️ Error : \`${err.toString()}\``);
+        });
+
+        if (error) return; 
+        await sendMessage(`Avatar updated successfully!`);
+
+    }
+}
