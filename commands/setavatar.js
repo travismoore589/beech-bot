c3cf41e42951309e6cb4a8bc47cec2f12735f42a
travismoore589 <68181253+travismoore589@users.noreply.github.com>
2024-03-07 09:53:16 -0500

    Update setavatar.js

--- commands/setavatar.js
+++ commands/setavatar.js
@@ -1,11 +1,13 @@
-const interactionHandlers = require('../modules/interaction-handlers.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('setavatar')
-    .setDescription('Set the avatar of the bot')
-    .addAttachmentOption(option => option.setName('avatar').setDescription('An image to use as the avatar for the bot').setRequired(true)),
+        .setName('setavatar')
+        .setDescription('Set the avatar of the bot')
+        .addAttachmentOption(option => 
+            option.setName('avatar')
+                .setDescription('An image to use as the avatar for the bot')
+                .setRequired(true)),
     async execute (interaction, client) {
 
         const { options } = interaction;
