ce919a7e455cbe0839c953ade1fe739373299780
travismoore589 <68181253+travismoore589@users.noreply.github.com>
2024-03-07 10:29:15 -0500

    Update setavatar.js for interaction-handler

--- commands/setavatar.js
+++ commands/setavatar.js
@@ -1,3 +1,4 @@
+const interactionHandlers = require('../modules/interaction-handlers.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
@@ -9,29 +10,6 @@ module.exports = {
                 .setDescription('An image to use as the avatar for the bot')
                 .setRequired(true)),
     async execute (interaction, client) {
-
-        const { options } = interaction;
-        const avatar = options.getAttachment('avatar');
-
-        async function sendMessage (message) {
-            const embed = new EmbedBuilder()
-            .setColor("Blurple")
-            .setDescription(message);
-
-            await interaction.reply({ embeds: [embed], ephermeral: true });
-        }
-
-        if (interaction.user.id !== '607017300989706240, 641147500908118016') return await interaction.reply({ content: `You do not have permission to use this command!`, ephermeral: true });
-
-        var error; 
-        await client.user.setAvatar(avatar.url).catch(async err => {
-            error = true; 
-            console.log(err);
-            return await sendMessage(`⚠️ Error : \`${err.toString()}\``);
-        });
-
-        if (error) return; 
-        await sendMessage(`Avatar updated successfully!`);
-
+        await interactionHandlers.avatarHandler(interaction);
     }
-}
+};
