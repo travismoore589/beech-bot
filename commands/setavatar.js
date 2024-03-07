654a410d38bc8aa1c8ae82646519a55338b7d835
travismoore589 <68181253+travismoore589@users.noreply.github.com>
2024-03-07 09:46:08 -0500

    Updated required const setavatar,js

--- commands/setavatar.js
+++ commands/setavatar.js
@@ -1,5 +1,5 @@
 const interactionHandlers = require('../modules/interaction-handlers.js');
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
