bea27cca112b777b6581a9f9c58bb50886d0d685
travismoore589 <68181253+travismoore589@users.noreply.github.com>
2025-09-07 11:04:53 -0400

    Update interaction-handlers.js
    add downloadHandler
    edit deleteHandler

--- modules/interaction-handlers.js
+++ modules/interaction-handlers.js
@@ -3,6 +3,37 @@ const queries = require('../database/queries.js');
 
 module.exports = {
 
+        downloadHandler: async (interaction, guildManager) => {
+        console.info(`DOWNLOAD command invoked by guild: ${interaction.guildId}`);
+        await interaction.deferReply({ ephemeral: true });
+        let content = '';
+        try {
+            const allQuotesFromServer = await queries.fetchAllQuotes(interaction.guildId);
+            if (allQuotesFromServer.length === 0) {
+                await interaction.followUp('There haven\'t been any quotes saved from this server, so I didn\'t attach a file.');
+                return;
+            }
+            for (const quote of allQuotesFromServer) {
+                content += await utilities.formatQuote(
+                    quote,
+                    true,
+                    true,
+                    guildManager,
+                    interaction
+                ) + '\n';
+            }
+            const buffer = Buffer.from(content);
+            await interaction.followUp({
+                files: [new AttachmentBuilder(buffer, { name: 'quotes.txt' })],
+                content: 'Here you go <:kermitGape:1406651455351558315> all the quotes saved from this server!',
+                ephemeral: false
+            });
+        } catch (e) {
+            console.error(e);
+            await interaction.followUp({ content: responseMessages.GENERIC_INTERACTION_ERROR, ephemeral: true });
+        }
+    },
+    
     addHandler: async (interaction) => {
         const author = interaction.options.getString('author').trim();
         const quote = interaction.options.getString('quote').trim();
@@ -82,19 +113,55 @@ module.exports = {
     },
 
     deleteHandler: async (interaction) => {
-        const result = await queries.deleteQuoteById(interaction.options.getInteger('identifier'), interaction.guildId).catch(async (e) => {
-            await interaction.reply(responseMessages.GENERIC_ERROR);
-        });
-
-        if (!interaction.replied) {
-            if (result.length === 0) {
-                await interaction.reply(responseMessages.NOTHING_DELETED);
-            } else {
-                await interaction.reply('The following quote was deleted: \n\n' + formatQuote(result[0], true, false));
-            }
+        await interaction.deferReply();
+        console.info(`DELETE command invoked by guild: ${interaction.guildId}`);
+        let searchResults;
+        try {
+            searchResults = await utilities.getQuoteSearchResults(interaction);
+        } catch (e) {
+            console.error(e);
+            await interaction.followUp({ content: responseMessages.GENERIC_INTERACTION_ERROR });
+            return;
         }
-    }
-};
+        if (searchResults.length === 0) {
+            await interaction.followUp(responseMessages.EMPTY_QUERY);
+            return;
+        } else if (searchResults.length > constants.MAX_DELETE_SEARCH_RESULTS) {
+            await interaction.followUp(responseMessages.DELETE_QUERY_TOO_GENERAL);
+            return;
+        }
+        const replyComponents = await utilities.buildDeleteInteraction(searchResults);
+        if (replyComponents.replyText.length > constants.MAX_DISCORD_MESSAGE_LENGTH) {
+            await interaction.followUp({ content: responseMessages.DELETE_SEARCH_RESULT_TOO_LONG });
+            return;
+        }
+        const response = await interaction.followUp({
+            content: replyComponents.replyText,
+            components: [new ActionRowBuilder().addComponents(replyComponents.buttons)]
+        });
+        const collectorFilter = i => i.user.id === interaction.user.id;
+        try {
+            const choice = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+            queries.deleteQuoteById(choice.customId, interaction.guildId)
+                .then(async (result) => {
+                    if (result.length === 0) {
+                        await choice.update({ content: responseMessages.NOTHING_DELETED, components: [] });
+                    } else {
+                        await choice.update({
+                            content: 'The following quote was deleted: \n\n' +
+                                await utilities.formatQuote(result[0], true),
+                            components: []
+                        });
+                    }
+                })
+                .catch(async (e) => {
+                    console.error(e);
+                    await choice.update({ content: responseMessages.GENERIC_INTERACTION_ERROR, ephemeral: false, components: [] });
+                });
+        } catch (e) {
+            await interaction.editReply({ content: 'A quote was not chosen within 60 seconds, so I cancelled the interaction.', components: [] });
+        }
+    },
 
 function formatQuote (quote, includeDate = true, includeIdentifier = false) {
     let quoteMessage = '';
